bcb71246868c7ea6c9433d01d3e122e533e9fe88
Mooni Don <mooncode610@gmail.com>
2016-08-31 13:56:20 -0700

    Format lines for display.

--- APM - Final Updated/app/shared/shared.module.ts
+++ APM - Final Updated/app/shared/shared.module.ts
@@ -4,11 +4,8 @@ import { CommonModule } from '@angular/common';
 import { StarComponent } from './star.component';
 
 @NgModule({
-  imports: [ CommonModule ],
-  exports : [
-    CommonModule,
-    StarComponent
-  ],
+  imports: [ CommonModule],
+  exports : [ StarComponent ],
   declarations: [ StarComponent ],
 })
 export class SharedModule { }
